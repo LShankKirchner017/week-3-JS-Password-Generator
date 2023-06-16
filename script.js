223c387044f518e28515766cc88a57c95ff74651
Lindsay <lshank017@gmail.com>
2023-06-16 09:02:29 -0500

    continued work on arrays

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
 
 var generateBtn = document.querySelector("#generate");
 
-var lowerCase = ["a", "b", "c"]
-var upperCase= ["A", "B", "C"]
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 
 var generatePassword = function () {
-  var userSelections = []
+  var userSelections = [];
   // How long do you want to password to be? (Prompt)
-    // store length in var
+  // store length in var
   // Do you want lower case? (Confirm)
-  var wantsLowerCase = confirm ("Do you want lower case?")
-    // If yes
-    if (wantsLowerCase) {
-      //merge lowerCase array into userSelections array
-      userSelections = userSelections.concat(lowerCase);
-    }
-    
-// If no, do nothing
+  var wantsLowerCase = confirm("Do you want lower case?");
+  // If yes
+  if (wantsLowerCase) {
+    //merge lowerCase array into userSelections array
+    userSelections = userSelections.concat(lowerCase);
+  }
+  // If no, do nothing
+
   // Do you want upper case? (Confirm)
+  var wantsUpperCase = confirm("Do you want upper case?");
+  // If yes
+  if (wantsUpperCase) {
+    //merge upperCase array into userSelections array
+    userSelections = userSelections.concat(upperCase);
+  }
 
-   var wantsUpperCase = confirm("Do you want upper case?");
-   // If yes
-   if (wantsUpperCase) {
-     //merge lowerCase array into userSelections array
-     userSelections = userSelections.concat(upperCase);
-   }
-
-    // If no, do nothing
+  // If no, do nothing
   // Do you want numbers? (Confirm)
-   // If yes
-      //merge  numbers array into userSelections array
-    // If no, do nothing
+  var wantsNumbers = confirm("Do you want numbers?");
+  // If yes
+  if (wantsNumbers) {
+    //merge upperCase array into userSelections array
+    userSelections = userSelections.concat(numbers);
+  }
+    
+  //merge  numbers array into userSelections array
+  // If no, do nothing
   // Do you want special characters? (Confirm)
-     // If yes
-      //merge specialChars array into userSelections array
-    // If no, do nothing
+  // If yes
+  //merge specialChars array into userSelections array
+  // If no, do nothing
 
-console.log(userSelections)
+  console.log(userSelections);
 
   // THEN I am presented with a series of prompts for password criteria
   //WHEN prompted for the length of the password
