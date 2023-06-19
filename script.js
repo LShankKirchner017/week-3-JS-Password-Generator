fd0b87501dacdbecd8e9040aaac8412b2c8754f8
Lindsay <lshank017@gmail.com>
2023-06-19 08:27:40 -0500

    finished arrays, cleaned up code, added notes

--- script.js
+++ script.js
@@ -6,45 +6,53 @@ var upperCase= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var specialChars = ["!", "@", "#", "$", "%", "^", ]
 
-
+// Where I am stuck: getting the length of the password as a var, generating the userSelections into a string with all the vars, generating password on screen
 var generatePassword = function () {
   var userSelections = [];
   // How long do you want to password to be? (Prompt)
-  // store length in var
-  // Do you want lower case? (Confirm)
+  var passwordLength = prompt("How long do you want your password to be? (8 - 128 characters).")
+
+
   var wantsLowerCase = confirm("Do you want lower case?");
-  // If yes
+ 
   if (wantsLowerCase) {
-    //merge lowerCase array into userSelections array
     userSelections = userSelections.concat(lowerCase);
   }
-  // If no, do nothing
 
-  // Do you want upper case? (Confirm)
   var wantsUpperCase = confirm("Do you want upper case?");
-  // If yes
   if (wantsUpperCase) {
-    //merge upperCase array into userSelections array
     userSelections = userSelections.concat(upperCase);
   }
 
-  // If no, do nothing
-  // Do you want numbers? (Confirm)
   var wantsNumbers = confirm("Do you want numbers?");
-  // If yes
   if (wantsNumbers) {
-    //merge upperCase array into userSelections array
     userSelections = userSelections.concat(numbers);
   }
-    
-  //merge  numbers array into userSelections array
-  // If no, do nothing
-  // Do you want special characters? (Confirm)
-  // If yes
-  //merge specialChars array into userSelections array
-  // If no, do nothing
 
+  var wantsSpecialChar = confirm("Do you want special characters?");
+  if (wantsSpecialChar) {
+    userSelections = userSelections.concat(specialChars);
+  }
   console.log(userSelections);
+
+
+  for (var i = 0, i < passwordLength; i++){
+    var rand = Math.floor(Math.random() * userSelections.length)
+    console.log(userSelections[rand])
+  }
+};
+// Write password to the #password input
+
+function writePassword() {
+  var password = generatePassword()
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
   // THEN I am presented with a series of prompts for password criteria
   //WHEN prompted for the length of the password
@@ -61,20 +69,7 @@ var generatePassword = function () {
   // WHEN the password is generated
   // THEN the password is either displayed in an alert or written to the page
   // Assignment Code
-};
 
-// Write password to the #password input
-
-function writePassword() {
-  var password = generatePassword()
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 
 
